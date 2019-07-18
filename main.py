085075e2a4dd6ed2260d5b14f039f5ed5fe8801c
Rafael <rpoliselit@gmail.com>
2019-07-17 22:59:56 -0300

    adding ball

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 #:kivy 1.11.1
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
 
 class PongGame(Widget):
     pass
 
 
+class PongBall(Widget):
+    velocity_x, velocity_y = NumericProperty(0), NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
 class PongApp(App):
     def build(self):
         return PongGame()
+
 
 if __name__ == '__main__':
     PongApp().run()
